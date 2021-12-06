881355fd6ae0bf79fde6b5481364ce26d9e1578d
AmitManna99 <manna.amit99@gmail.com>
2021-12-06 10:51:50 +0530

    ongoing time updated

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ const days = {
 const showMeet = (d,h,m) => {
     let day = days[d];
     let dayCTX = document.querySelector('#'+day);
-    if((h==9 && m>=0) || (h==9 && m<=50)) {
+    if((h==9 && m>=55) || (h==10 && m<=49)) {
         dayCTX.childNodes[3].className= "ongoing";
     }
-    if((h==9 && m>=55) || (h==10 && m<=40)) {
+    if((h==10 && m>=50) || (h==11 && m<=44)) {
         dayCTX.childNodes[5].className= "ongoing";
     }
-    if((h==10 && m>=45) || (h==11 && m<=45)) {
+    if((h==12 && m>=25) || (h==13 && m<=19)) {
         dayCTX.childNodes[7].className= "ongoing";
     }
-    if((h==12 && m>=25) || (h==13 && m<=15)) {
+    if((h==13 && m>=20) || (h==14 && m<=14)) {
         dayCTX.childNodes[9].className= "ongoing";
     }
-    if((h==13 && m>=20) || (h==16 && m<=55)) {
+    if((h==14 && m>=15) || (h==16 && m<=55)) {
         dayCTX.childNodes[11].className= "ongoing";
     }
 }
